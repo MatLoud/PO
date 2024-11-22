927fa26e68cc7419af33f2494dd275a684238c3e
Mateo Fernández <matefer514@gmail.com>
2024-11-22 10:04:57 -0500

    calculadora en po

---
+++ Clase 1/src/clase/pkg1/Calculadora.java
@@ -0,0 +1,11 @@
+
+package clase.pkg1;
+public class Calculadora {
+public int a,b;
+public Calculadora(int a, int b){
+ this.a = a;
+ this.b = b;
+}
+public int suma(){ return a+b;}
+
+}
---
+++ Clase 1/src/clase/pkg1/Clase1.java
@@ -0,0 +1,11 @@
+
+package clase.pkg1;
+
+public class Clase1 {
+public static void main(String[] args) {
+        // logica
+   Calculadora cc = new Calculadora(5,2);
+   System.out.println("La suma de los numeros:"+cc.suma());
+    }
+    
+}
