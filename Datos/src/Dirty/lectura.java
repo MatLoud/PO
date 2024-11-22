5547243233106a72d6271ba9aa21eb0584735229
Mateo Fernández <matefer514@gmail.com>
2024-11-22 09:23:15 -0500

    Datos Basicos

--- Datos/src/Dirty/lectura.java
+++ Datos/src/Dirty/lectura.java
@@ -7,6 +7,11 @@ public class lectura {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Coloca una frase que te guste para empezar \n");
     String frase=scanner.nextLine();
+    System.out.print("Coloque su nombre: \n");
+    String nombre=scanner.nextLine();
+    System.out.print("Coloque su Cedula: \n");
+    int id=scanner.nextInt();
     System.out.println("Su frase es: "+frase+"\n");
+    System.out.println("Autor: "+nombre+"\n De Cedula de identidad:"+id+"\n");
     }
 }
