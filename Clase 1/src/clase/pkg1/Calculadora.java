4f3fe3281bd549cff8cd7bfc5f7250c782b5457c
Mateo Fernández <matefer514@gmail.com>
2024-11-25 09:00:06 -0500

    Exposición

--- Clase 1/src/clase/pkg1/Calculadora.java
+++ Clase 1/src/clase/pkg1/Calculadora.java
@@ -1,11 +1,21 @@
 
 package clase.pkg1;
+
 public class Calculadora {
-public int a,b;
-public Calculadora(int a, int b){
+public double a,b;
+public Calculadora(double a, double b){
  this.a = a;
  this.b = b;
 }
-public int suma(){ return a+b;}
-
+public double suma(){ return a+b;}
+public double resta(){ return a-b;}
+public double mult(){ return a*b;}
+public double div(){  
+    if (b!=0) {
+            return a/b;
+        } else {
+            System.out.println("Error: División entre cero no permitida.");
+            return 0;
+        }
+}
 }
--- Clase 1/src/clase/pkg1/Clase1.java
+++ Clase 1/src/clase/pkg1/Clase1.java
@@ -1,11 +1,33 @@
 
 package clase.pkg1;
-
+import java.util.Scanner;
 public class Clase1 {
 public static void main(String[] args) {
-        // logica
-   Calculadora cc = new Calculadora(5,2);
-   System.out.println("La suma de los numeros:"+cc.suma());
-    }
-    
+// logica
+Scanner leer= new Scanner(System.in);
+  int terminar=0;
+  while(terminar==0) {
+    Limpiar.limpiarPantalla(); 
+    System.out.println("-----------------------------\nSeleccione la operacion:");
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Dividir");
+        System.out.println("Coloque cualquier otro numero para salir del programa...\n-----------------------------");
+    int opcion = leer.nextInt();
+    if (opcion>=5 || opcion<=0){
+    System.exit(0);}
+    System.out.println("Coloque sus dos numeros a operar:");
+    double a=leer.nextInt();
+    double b=leer.nextInt();
+    Calculadora cc = new Calculadora(a,b);
+     switch (opcion){
+        case 1: System.out.println("\nLa Suma de los numeros:"+cc.suma()); break; 
+        case 2: System.out.println("\nLa Resta de los numeros:"+cc.resta()); break;
+        case 3: System.out.println("\nLa Multiplicacion de los numeros:"+cc.mult()); break;
+        case 4: System.out.println("\nLa Division de los numeros:"+cc.div()); break;
+        default: terminar=1;
+     }
+   }
+ }
 }
